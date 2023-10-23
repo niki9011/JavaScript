9276c252769b3e989a684455d506ec71c991d69a
niki9011 <niki901115@gmail.com>
2023-10-24 00:02:56 +0300

    add 2, 3 tasks

---
+++ JS Syntax Fundamentals – Lab/2.Student Information.js
@@ -0,0 +1,5 @@
+function student (name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}.`);
+}
+
+student('Marry', 12, 6.00)
---
+++ JS Syntax Fundamentals – Lab/3.Excellent Grade.js
@@ -0,0 +1,10 @@
+function exellentGrade(grade) {
+    if (grade >= 5.50) {
+        console.log('Exellent');
+    } else {
+    console.log('Not exellent');
+    }
+}
+
+exellentGrade(5.50)
+exellentGrade(4.35)
