51c7ee015ed2faac66acb8746d3c83dbfbf286d7
niki9011 <niki901115@gmail.com>
2023-10-25 00:38:15 +0300

    add 4,5,6 tasks

---
+++ JS Syntax Fundamentals – Lab/05.Math Operations.js
@@ -0,0 +1,32 @@
+function mathOperations(num1, num2, operator){ 
+     let result; 
+    
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+        
+        case '/':
+            result = num1 / num2;
+            break;
+        
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = num1 ** num2;
+            break;
+    }
+    console.log(result);
+}
+
+mathOperations(2, 3, '+')
+mathOperations(3, 5.5, '*')
