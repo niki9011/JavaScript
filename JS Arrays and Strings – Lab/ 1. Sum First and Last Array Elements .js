5ed0d82a0ba7033f8017e8c01c4dd6f8c16a907b
niki9011 <niki901115@gmail.com>
2023-10-30 22:54:05 +0200

    1-6 tasks add in JS Arrays and Strings – Lab

---
+++ JS Arrays and Strings – Lab/ 1. Sum First and Last Array Elements .js
@@ -0,0 +1,7 @@
+function sumFirstLast(args) {
+    let sum = args[0] + args[args.length - 1];
+    console.log(sum);
+}
+sumFirstLast([20, 30, 40])
+sumFirstLast([10, 17, 22, 33])
+sumFirstLast([11, 58, 69])
---
+++ JS Arrays and Strings – Lab/5. Censored Words.js
@@ -0,0 +1,8 @@
+function censoredWord(text, word) {
+    while (text.includes(word)) {
+        text = text.replace(word, '*'.repeat(word.length));
+    }
+    console.log(text);
+}
+censoredWord('A small sentence with some words', 'small');
+censoredWord('Find the hidden word', 'hidden');
