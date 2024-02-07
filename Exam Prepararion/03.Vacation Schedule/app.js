b524a64a0fde8800185d31e1e723e6c575ede8b8
niki9011 <niki901115@gmail.com>
2024-02-07 21:19:19 +0200

    function renderVacations

--- Exam Prepararion/03.Vacation Schedule/app.js
+++ Exam Prepararion/03.Vacation Schedule/app.js
@@ -1,39 +1,47 @@
-const baseUrl = 'http://localhost:3030/jsonstore/tasks/'
-const loadVacationsButton = document.getElementById('load-vacations');
-const vacationsList = document.getElementById('list');
+const baseUrl = "http://localhost:3030/jsonstore/tasks/";
+const loadVacationsButton = document.getElementById("load-vacations");
+const vacationsList = document.getElementById("list");
 
-loadVacationsButton.addEventListener('click', (e) => {
-    fetch(baseUrl)
-    .then(res => res.json())
-    .then(result => {console.log(result);})
+loadVacationsButton.addEventListener("click", (e) => {
+  fetch(baseUrl)
+    .then((res) => res.json())
+    .then((result) => {
+      renderVacations(Object.values(result));
+    });
 });
 
-function renderVacations(vacations){
-
+function renderVacations(vacations) {
+  vacations
+    .map(renderVacation)
+    .forEach((vacationElement) => vacationsList.appendChild(vacationElement));
 }
 
-function renderVacation(vacations){
-    const container = document.createElement('div');
-    container.className = 'container';
+function renderVacation(vacation) {
+  const container = document.createElement("div");
+  container.className = "container";
 
-    const h2Element = document.createElement('h2');
-    h2Element.textContent = vacations.name;
+  const h2Element = document.createElement("h2");
+  h2Element.textContent = vacation.name;
 
+  const h3DateElement = document.createElement("h3");
+  h3DateElement.textContent = vacation.date;
 
-    const h3DateElement =  document.createElement('h3');
-    h3DateElement.textContent  = vacations.date;
+  const h3DaysElement = document.createElement("h3");
+  h3DaysElement.textContent = vacation.days;
 
+  const changeButton = document.createElement("button");
+  changeButton.className = "change-btn";
+  changeButton.textContent = "Change";
 
-    const h3DaysElement = document.createElement('h3');
-    h3DaysElement = vacations.days;
+  const doneButton = document.createElement("button");
+  doneButton.className = "done-btn";
+  doneButton.textContent = "Done";
 
+  container.appendChild(h2Element);
+  container.appendChild(h3DateElement);
+  container.appendChild(h3DaysElement);
+  container.appendChild(changeButton);
+  container.appendChild(doneButton);
 
-    const changeButton = document.createElement('button');
-    changeButton.className = 'change-btn';
-
-    const doneButton = document.createElement('button');
-    doneButton.className = 'done-btn';
-
-
-    
+  return container;
 }
